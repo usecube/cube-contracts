f858bc69e2b259cbde854f0bd2fb48bf970e1b54
Dann Wee <dannweezj@gmail.com>
2024-10-12 12:58:01 +0800

    feat: update vault contract to be aave compatible and also update exchange contract

--- script/deploy/Vault.s.sol
+++ script/deploy/Vault.s.sol
@@ -1,22 +1,47 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {Vault} from "../../src/Vault.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 contract DeployVault is Script {
     function run() external {
+        // Address of USDC on Base Sepolia
         address usdcAddress = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+
+        // Address of Aave V3 Pool on Base Sepolia
+        address aaveV3PoolAddress = 0xbE781D7Bdf469f3d94a62Cdcc407aCe106AEcA74;
+
+        // Address of aBaseUSDC on Base Sepolia
+        address aBaseUSDCAddress = 0xfE45Bf4dEF7223Ab1Bf83cA17a4462Ef1647F7FF;
 
         vm.startBroadcast();
 
-        Vault vault = new Vault(IERC20(usdcAddress));
+        // Deploy the implementation contract
+        Vault vaultImplementation = new Vault();
+
+        // Deploy ProxyAdmin with the sender as the owner
+        ProxyAdmin proxyAdmin = new ProxyAdmin(msg.sender);
+
+        // Encode the initialization call
+        bytes memory initializeData =
+            abi.encodeWithSelector(Vault.initialize.selector, IERC20(usdcAddress), aaveV3PoolAddress, aBaseUSDCAddress);
+
+        // Deploy the proxy
+        TransparentUpgradeableProxy proxy =
+            new TransparentUpgradeableProxy(address(vaultImplementation), address(proxyAdmin), initializeData);
+
+        // The proxy address is the address of our upgradeable Vault
+        Vault vault = Vault(address(proxy));
 
         vm.stopBroadcast();
 
-        console.log("Vault deployed at:", address(vault));
+        console.log("Vault implementation deployed at:", address(vaultImplementation));
+        console.log("ProxyAdmin deployed at:", address(proxyAdmin));
+        console.log("Vault proxy deployed at:", address(vault));
     }
 }
--- src/Exchange.sol
+++ src/Exchange.sol
@@ -6,20 +6,25 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Registry} from "./Registry.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 contract Exchange is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     Registry public immutable registry;
     IERC20 public immutable usdcToken;
+    IERC4626 public immutable vault;
 
     event Transfer(address indexed from, address indexed to, uint256 amount, string uen);
+    event VaultDeposit(address indexed from, address indexed to, uint256 amount, uint256 shares, string uen);
 
-    constructor(address _registryAddress, address _usdcAddress) {
+    constructor(address _registryAddress, address _usdcAddress, address _vaultAddress) {
         require(_registryAddress != address(0), "Invalid registry address");
         require(_usdcAddress != address(0), "Invalid USDC address");
+        require(_vaultAddress != address(0), "Invalid vault address");
         registry = Registry(_registryAddress);
         usdcToken = IERC20(_usdcAddress);
+        vault = IERC4626(_vaultAddress);
     }
 
     function transfer(string memory _uen, uint256 _amount) external nonReentrant {
@@ -33,6 +38,20 @@ contract Exchange is ReentrancyGuard {
         emit Transfer(msg.sender, merchant.wallet_address, _amount, _uen);
     }
 
+    function transferToVault(string memory _uen, uint256 _amount) external nonReentrant {
+        require(_amount > 0, "Amount must be greater than zero");
+
+        Registry.Merchant memory merchant = registry.getMerchantByUEN(_uen);
+        require(merchant.wallet_address != address(0), "Invalid merchant wallet address");
+
+        usdcToken.safeTransferFrom(msg.sender, address(this), _amount);
+        usdcToken.safeIncreaseAllowance(address(vault), _amount);
+
+        uint256 shares = vault.deposit(_amount, merchant.wallet_address);
+
+        emit VaultDeposit(msg.sender, merchant.wallet_address, _amount, shares, _uen);
+    }
+
     function getMerchantWalletAddress(string memory _uen) external view returns (address) {
         Registry.Merchant memory merchant = registry.getMerchantByUEN(_uen);
         return merchant.wallet_address;
@@ -40,5 +59,9 @@ contract Exchange is ReentrancyGuard {
 
     function getUSDCBalance(address _account) external view returns (uint256) {
         return usdcToken.balanceOf(_account);
+    }
+
+    function getVaultBalance(address _account) external view returns (uint256) {
+        return vault.balanceOf(_account);
     }
 }
